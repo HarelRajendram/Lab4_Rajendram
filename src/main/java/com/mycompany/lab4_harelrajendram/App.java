4c3a393e76d221e833b8bc9b6eae2bd3c4804641
HarelRajendram <2534297@edu.vaniercollege.qc.ca>
2026-09-14 09:25:57 -0400

    Added text fields to the lab

---
+++ src/main/java/com/mycompany/lab4_harelrajendram/App.java
@@ -0,0 +1,47 @@
+package com.mycompany.lab4_harelrajendram;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+    private TextField NumdaysTrip;
+        private TextField airfare ;
+        private TextField carRentalFee;
+        private TextField miles;
+        private TextField parkingFees;
+        private TextField taxiCharge;
+        private TextField registrationFees;
+        private TextField lodgingCharge;
+
+    @Override
+    public void start(Stage stage) {
+    
+        NumdaysTrip = new TextField();
+        airfare = new TextField();
+        carRentalFee = new TextField();
+        miles = new TextField();
+        parkingFees = new TextField();
+        taxiCharge = new TextField();
+        registrationFees = new TextField();
+        lodgingCharge = new TextField();
+        
+        Label NumdaysTrip = new Label("Number of days on the trip:");
+        
+        stage.setScene(scene);
+       
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
